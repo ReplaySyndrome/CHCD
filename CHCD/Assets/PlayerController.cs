f567899aed5fd05a7a7a8c6367930561e392810d
ReplaySyndrome <43131669+ReplaySyndrome@users.noreply.github.com>
2021-04-23 11:18:37 +0900

    ....

--- CHCD/Assets/PlayerController.cs
+++ CHCD/Assets/PlayerController.cs
@@ -15,14 +15,17 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(720, 1080, true);
+
+    
     }
 
     // Update is called once per frame
     void Update()
     {
         MouseAction();
-        
+        Screen.SetResolution(1920, 1080, true);
     }
 
     
---
+++ CHCD/Assets/Tower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    protected float cooltime = 3f;
+    protected GameObject target;
+    public Texture2D texture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
